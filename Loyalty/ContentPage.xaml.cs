57c6ba389ae997106c1c633dcb22455929ee7dfb
JuneCho23 <junecho.ucb@gmail.com>
2017-04-19 10:05:30 -0700

    XAML

---
+++ Loyalty/ContentPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Loyalty
+{
+	public partial class ContentPage : ContentPage
+	{
+		public ContentPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
